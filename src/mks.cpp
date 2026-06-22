89c615916cbef860c98fc7cb7c39467512c6daf8
SpadeEpsilonDelta <captain.bayona@gmail.com>
2026-06-22 04:43:44 -0400

    Modified mks.cpp with headers in mks.h to include commands to return the current angle (already usable in main) as well as working on getting motor to respond accurately with moveToAngle() and syncMotor() functions, which remain in progress.

--- src/mks.cpp
+++ src/mks.cpp
@@ -1,5 +1,12 @@
 #include "mks.h"
 #include "commandLink.h"
+#include <thread>
+#include <vector>
+#include <cmath>
+
+#include <string>
+#include "tools.h"
+
 
 
 Motor::Motor(CommandLink& l) : deviceFunctions(l){
@@ -12,7 +19,7 @@ void Motor::start() {
         link.sendCommand(startServo, sizeof(startServo)); 
     }
 void Motor::stop() {
-        //std::cout << "Stopping motor on " << link.getPort() << "\n";
+        std::cout << "\n" << "Stopping motor on " << link.getPort();
         link.sendCommand(stopServo, sizeof(stopServo)); 
     }
 
@@ -22,6 +29,81 @@ void Motor::calibrate_Servo(){
         link.sendCommand(calibrateServo, sizeof(calibrateServo)); 
         link.readCommand(0xE1); 
     }
-void Motor::test(){
-        link.sendCommand(Servotest, sizeof(Servotest)); 
+
+int32_t Motor::getCurrentAngle(){
+
+        auto currentAngle_raw = link.parseMKShex(6, 0xE0); 
+        int32_t raw_angle = 
+                static_cast<int32_t>(((uint32_t)currentAngle_raw[1] << 24) |
+                          ((uint32_t)currentAngle_raw[2] << 16) |
+                          ((uint32_t)currentAngle_raw[3] << 8)  |
+                          ((uint32_t)currentAngle_raw[4]));
+            float currentAngle = (static_cast<float>(raw_angle % 65536) * 360.0f) / 65536.0f;
+            currentAngle = abs(currentAngle);
+            std::cout << " Raw Angle: " << raw_angle << "ticks" << "\n"; 
+            std::cout << " Current Angle: " << currentAngle << "deg" << "\n"; 
+        return raw_angle; 
+}
+
+void Motor::moveToAngle(int32_t angle, char dir){
+    const int stepsPerRev = 200; //# of steps per full rev (1.8deg motor)
+    const int M_step = 16; //subdivisions  
+    auto currentAngle1 = getCurrentAngle(); //int32_t 
+    int32_t angleInTicks = angleToTicks(angle); 
+    std::this_thread::sleep_for(25ms); 
+    std::vector<uint8_t> goToCommand = {0xE0, 0xFD}; 
+    uint8_t forwardSpeedByte = 0x08; //dir forward, gear 8 
+    uint8_t reverseSpeedByte1 = 0x88; //dir reverse
+    uint8_t direction{0}; 
+
+    switch (dir)
+    {
+    case ('F'):
+        direction = forwardSpeedByte; 
+        break; 
+    case ('R'):
+        direction = reverseSpeedByte1; 
+        break;
+    default:
+        //base case 
+        break;
     }
+
+    goToCommand.push_back(direction);
+    int32_t delta = (int32_t)angleInTicks - currentAngle1; 
+    uint32_t pulse = (delta * stepsPerRev * M_step) / 360; 
+    uint8_t pulse_byte[4];
+    pulse_byte[0] = (uint8_t)(pulse >> 24) & 0xFF;
+    pulse_byte[1] = (uint8_t)(pulse >> 16) & 0xFF;
+    pulse_byte[2] = (uint8_t)(pulse >> 8) & 0xFF;
+    pulse_byte[3] = (uint8_t)(pulse & 0xFF);
+    for(int j=0; j<4; j++){goToCommand.push_back(pulse_byte[j]);} 
+
+    uint8_t tCHK{0}; 
+    for(int j=0; j<7; j++){
+        tCHK = tCHK + goToCommand[j]; 
+    }
+    tCHK = (tCHK % 256) & 0xFF; 
+    goToCommand.push_back(tCHK); //append with checksum 
+    link.sendCommand(goToCommand.data(), goToCommand.size());
+    std::this_thread::sleep_for(250ms); 
+    link.sendCommand(stopServo,sizeof(stopServo)); //stop servo once angle is reached
+    std::this_thread::sleep_for(1000ms); 
+}
+
+void Motor::syncMotor(bool newScan, uint32_t desiredAngle, char direction){    
+    moveToAngle(desiredAngle, direction); 
+
+}
+
+void Motor::gotoZero()
+{
+    int32_t currentAngle = getCurrentAngle();
+    if (currentAngle == 0) {
+        return;
+    }
+    if (currentAngle > 180) {
+        moveToAngle(360, 'F');} 
+    else {
+        moveToAngle(0, 'R');}
+}
--- src/mks.h
+++ src/mks.h
@@ -3,19 +3,31 @@
 
 class Motor : public deviceFunctions{
 private:
-    //Servo Commands 
-    uint8_t uartMode[4] = {0xE0, 0x82, 0x02, 0x63}; //UART mode
-    uint8_t calibrateServo[4] = {0xE0,0x80, 0x00, 0x60}; //must wait for calibration to complete first 
-    uint8_t startServo[4] = {0xE0, 0xF6, 0x90, 0x66}; //starts 
-    uint8_t Servotest[4] = {0xE0, 0xF6, 0x01, 0xD7}; //right now this command i have reverses the servo max speed
-    uint8_t stopServo[3] = {0xE0, 0xF7, 0xD7}; //stops motor 
-
+    //SEND Commands 
+    uint8_t uartMode[4] = {0xE0, 0x82, 0x02, 0x63}; 
+    uint8_t calibrateServo[4] = {0xE0,0x80, 0x00, 0x60}; 
+    uint8_t startServo[4] = {0xE0, 0xF6, 0x90, 0x66}; 
+    uint8_t forwardSlow[4] = {0xE0, 0xF6, 0x01, 0xD7};
+    uint8_t reverseSlow[4] = {0xE0, 0xF6, 0x81, 0x57}; 
+    uint8_t stopServo[3] = {0xE0, 0xF7, 0xD7};
+    uint8_t M_Step_16[4] = {0xE0, 0x84, 0x10, 0x74}; 
+    uint8_t M_Step_32[4] = {0xE0, 0x84, 0x20, 0x84}; 
+    uint8_t goToZero[4] = {0xE0, 0x91, 0x00, 0x71};
+    uint8_t go180[8] = {0xE0, 0xFD, 0x02, 0x00, 0x00, 0x06, 0x40, 0x31};
+    uint8_t motorCW[4] = {0xE0, 0x86, 0x00, 0x66}; 
+    uint8_t motorCCW[4] = {0xE0, 0x86, 0x01, 0x67};
+    
+    //Send command with 32bit return values 
+    uint8_t readAngle[3] = {0xE0, 0x36, 0x16}; //Returns e0 , angle(uint32_t),rCHK.
+                                               //uint32_t can be:  0~65535 for 1 full rotation 
 public:
     Motor(CommandLink& l);
-
-    //functions 
     void start();
     void stop();
     void calibrate_Servo();
-    void test();
+    int32_t getCurrentAngle(); 
+    void moveToAngle(int32_t angle, char dir); 
+    void gotoZero(); 
+    void syncMotor(bool newScan, uint32_t desiredAngle, char direction);
+
 };
